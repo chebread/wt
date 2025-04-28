41cb68fad83c15978a157728d23363df290d2978
chebread <83535893+chebread@users.noreply.github.com>
2025-04-28 21:48:23 +0900

    feat: MVP

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-print(">>> wt <<<")
+print("-- Write Text --\n-- <q: quit> --")
 n = 1
 storage = "" # default value
-while (user_input := input()) != "q":
+while (user_input := input(f"{n}: ")) != "q":
   # user_input은 계속 현재 값으로 초기화 됩니다
   n += 1
   storage += user_input + "\n"
-print(">>> quit <<<")
